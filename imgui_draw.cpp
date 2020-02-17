fe481c2946acf5d40c0e7d26d4eaff9a7c7f1dc9
Omar <omarcornut@gmail.com>
2020-02-17 11:58:22 +0100

    Texture-based AA lines: Fixes for AddCustomRect api, add IMGUI_HAS_TEXLINES define (temporarily) to facilitate working with test cases, Demo allows growing FrameBorderSize for testing

--- imgui_draw.cpp
+++ imgui_draw.cpp
@@ -2344,16 +2344,14 @@ static void ImFontAtlasBuildRenderDefaultTexData(ImFontAtlas* atlas)
     atlas->TexUvWhitePixel = ImVec2((r.X + 0.5f) * atlas->TexUvScale.x, (r.Y + 0.5f) * atlas->TexUvScale.y);
 }
 
-const unsigned int FONT_ATLAS_AA_LINE_TEX_DATA_ID = 0x80010000;
-
 static void ImFontAtlasBuildRegisterAALineCustomRects(ImFontAtlas* atlas)
 {
     if ((atlas->Flags & ImFontAtlasFlags_NoAALines))
         return;
 
-    const int max_width = IM_DRAWLIST_TEX_AA_LINES_WIDTH_MAX; // The maximum line width we want to generate
     // The "max_width + 2" here is to give space for the end caps, whilst height (IM_DRAWLIST_TEX_AA_LINES_WIDTH_MAX+1) is to accommodate the fact we have a zero-width row
-    atlas->AALineRectId = atlas->AddCustomRectRegular(FONT_ATLAS_AA_LINE_TEX_DATA_ID, max_width + 2, IM_DRAWLIST_TEX_AA_LINES_WIDTH_MAX + 1);
+    const int max_width = IM_DRAWLIST_TEX_AA_LINES_WIDTH_MAX; // The maximum line width we want to generate
+    atlas->AALineRectId = atlas->AddCustomRectRegular(max_width + 2, IM_DRAWLIST_TEX_AA_LINES_WIDTH_MAX + 1);
 }
 
 static void ImFontAtlasBuildRenderAALinesTexData(ImFontAtlas* atlas)
